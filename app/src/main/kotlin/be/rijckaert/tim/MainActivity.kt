b36f2485484dd09c091a3ac84737e0b5efa06e75
Tim Rijckaert <tim.rijckaert@vrt.be>
2017-05-12 14:45:01 +0200

    Trying to fix weird bug. Reproduced in sample

--- app/src/main/kotlin/be/rijckaert/tim/MainActivity.kt
+++ app/src/main/kotlin/be/rijckaert/tim/MainActivity.kt
@@ -1,36 +1,44 @@
 package be.rijckaert.tim
 
 import android.os.Bundle
+import android.os.Handler
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import be.rijckaert.tim.lib.CustomPullToRefreshLayout
+import java.lang.Thread.sleep
 import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
     val dataSource = (1..19).map { "I hate API $it" }.toTypedArray()
 
+    private val simpleAdapter = SimpleAdapter()
+
+    private val SIMULATED_NETWORK_DELAY = 2 * 1000L //ms
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val recyclerView = this.findViewById(R.id.recyclerView) as RecyclerView
 
-        with(recyclerView) {
-            layoutManager = LinearLayoutManager(this@MainActivity)
-            adapter = SimpleAdapter(dataSource)
-        }
+        (findViewById(R.id.swipe_refresh) as? CustomPullToRefreshLayout)?.let {
+            it.onRefreshListener = {
+                thread {
+                    sleep(SIMULATED_NETWORK_DELAY * 2)
+                    runOnUiThread {
+                        simpleAdapter.dataSource = (20..25).map { "I love API $it" }.toTypedArray()
+                        simpleAdapter.notifyDataSetChanged()
 
-        val pullToRefresh = findViewById(R.id.pullToRefresh) as CustomPullToRefreshLayout
-        pullToRefresh.onRefreshListener = {
-            thread {
-                //
-                Thread.sleep(5000)
-
-                runOnUiThread {
-                    pullToRefresh.setRefreshing(false, false)
+                        it.setRefreshing(false, false)
+                    }
                 }
             }
         }
+
+        Handler().postDelayed({
+            recyclerView.adapter = simpleAdapter
+            simpleAdapter.dataSource = dataSource
+            simpleAdapter.notifyDataSetChanged()
+        }, SIMULATED_NETWORK_DELAY)
     }
 }
--- app/src/main/kotlin/be/rijckaert/tim/SimpleAdapter.kt
+++ app/src/main/kotlin/be/rijckaert/tim/SimpleAdapter.kt
@@ -5,7 +5,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 
-class SimpleAdapter(private val dataSource: Array<String>) : RecyclerView.Adapter<SimpleAdapter.SimpleViewHolder>() {
+class SimpleAdapter : RecyclerView.Adapter<SimpleAdapter.SimpleViewHolder>() {
+    var dataSource: Array<String> = emptyArray()
     override fun getItemCount(): Int = dataSource.size
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SimpleViewHolder {
